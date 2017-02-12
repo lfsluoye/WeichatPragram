97362cde753b7bdbedc98c3dfb540ff1831fb9b1
lfs736012 <lfs736012@163.com>
2017-02-12 21:16:34 +0800

    techan

---
+++ Techan/pages/cart/cart.js
@@ -0,0 +1,22 @@
+Page({
+	data:{
+
+	},
+	onLoad:function(params){
+		console.log(params);
+
+		wx.request({
+		  url: 'test.php', //仅为示例，并非真实的接口地址
+		  data: {
+		     x: '' ,
+		     y: ''
+		  },
+		  header: {
+		      'content-type': 'application/json'
+		  },
+		  success: function(res) {
+		    console.log(res.data)
+		  }
+		})
+	}
+})
